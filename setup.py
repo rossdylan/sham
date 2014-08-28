d938ae3da43814c3c45ae95b6116bd87282c8691
Ross <rossdylan@csh.rit.edu>
2014-08-28 11:15:20 -0400

    version bump in setup.py to fix the pypi upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sham',
-      version='0.1.0',
+      version='0.1.1',
       description='Friendly interface to libvirt',
       author='Ross Delinger',
       author_email='ross.delinger@gmail.com',
